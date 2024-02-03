932945abdb5e1dd20f2b855ff1c7af272e44aed6
jiathehuman <jiathehuman@gmail.com>
2024-02-03 11:33:07 +0800

    finished task 1 to 7

--- sketch.js
+++ sketch.js
@@ -4,18 +4,27 @@
 // PLEASE NOTE: Due to compatibility issues with the face detection library
 // this example uses an older version of the P5js library
 
+
+// Commentary:
+/* ////////////////////////////////////////////////////////////////
+COMMENTARY
+//////////////////////////////////////////////////////////////////
+Segmentation for the red channel is a lot more accurate that green and red.
+*/
+
 var imgWidth = 160,
     imgHeight = 120;
 
 var webcam_stream; 
 var img;
 var greyscaleImg;
-var redImg;
-var greenImg;
-var blueImg;
+var redSegmentImg;
+var greenSegmentImg;
+var blueSegmentImg;
 
-var segmentationSlider 
-var segmentationVal
+var segmentationRSlider, segmentationGSlider, segmentationBSlider 
+
+var segmentationRVal, segmentationGVal, segmentationBVal
 
 var detector;
 var classifier = objectdetect.frontalface;
@@ -29,16 +38,25 @@ function preload(){
 }
 ////////////////////////////////////////////////////////////////
 function setup() {
-    createCanvas(imgWidth * 3, imgHeight * 5);
+    const canvas = createCanvas(imgWidth * 3, imgHeight * 5);
+    canvas.parent("canvas");
     pixelDensity(1); // makes sure it renders correctly on different screens
     webcam_stream = createCapture(VIDEO);
     webcam_stream.hide();
     webcam_stream.size(imgWidth, imgHeight);
 
-    segmentationSlider = createSlider(0, 255, 100);
-    segmentationSlider.position(50, imgHeight * 5 + 20)
+    segmentationRSlider = createSlider(0, 255, 100);
+    // segmentationRSlider.position(50, imgHeight * 5 + 20)
 
-    
+    segmentationGSlider = createSlider(0, 255, 100);
+    // segmentationGSlider.position(50, imgHeight * 5 + 40)
+
+    segmentationBSlider = createSlider(0, 255, 100);
+    // segmentationBSlider.position(50, imgHeight * 5 + 60)
+
+    segmentationRSlider.parent("redslider");
+    segmentationGSlider.parent("greenslider");
+    segmentationBSlider.parent("blueslider");
 
 }
 ///////////////////////////////////////////////////////////////
@@ -46,7 +64,9 @@ function draw() {
 
     background(0);
     // var sumX = 0, sumY = 0, avgX = 0, avgY = 0;
-    segmentationVal = segmentationSlider.value()
+    segmentationRVal = segmentationRSlider.value()
+    segmentationGVal = segmentationGSlider.value()
+    segmentationBVal = segmentationBSlider.value()
 
     if(imageLoaded)
     {
@@ -54,12 +74,22 @@ function draw() {
         greyscaleImg = greyscaleFilter(img);
 
         redImg = createImage(imgWidth, imgHeight);
-        redImg = channels(img, "red");
-
         greenImg = createImage(imgWidth, imgHeight);
-        greenImg = channels(img, "green");
         blueImg = createImage(imgWidth, imgHeight);
-        blueImg = channels(img, "blue");
+
+        redImg = channel(img, "red");
+        greenImg = channel(img, "green");
+        blueImg = channel(img, "blue");
+
+
+        redSegmentImg = createImage(imgWidth, imgHeight);
+        redSegmentImg = channelSegmentation(img, "red");
+
+        greenSegmentImg = createImage(imgWidth, imgHeight);
+        greenSegmentImg = channelSegmentation(img, "green");
+
+        blueSegmentImg = createImage(imgWidth, imgHeight); 
+        blueSegmentImg = channelSegmentation(img, "blue");
 
 
 
@@ -69,61 +99,11 @@ function draw() {
         image(greenImg, imgWidth, imgHeight);
         image(blueImg, imgWidth * 2, imgHeight);
 
-        
-        // image(greenImg, imgWidth, imgHeight);
-        // image(blueImg, imgWidth * 2, imgHeight);
 
+        image(redSegmentImg, 0, imgHeight * 2);
+        image(greenSegmentImg, imgWidth, imgHeight * 2);
+        image(blueSegmentImg, imgWidth * 2, imgHeight * 2);
 
-        // //////////////////
-        // var sumX=0, sumY=0, avgX=0, avgY = 0;
-        // var matchCount=0;
-
-        // var redTarget = 0;
-        // var greenTarget = 0;
-        // var blueTarget = 255;
-    
-        // for (var x = 0; x < img.width; x += 1) {
-        //     for (var y = 0; y < img.height; y += 1) {
-        //         var index = (x + (y * img.width)) * 4;
-        //         var redSource = img.pixels[index + 0];
-        //         var greenSource = img.pixels[index + 1];
-        //         var blueSource = img.pixels[index + 2];
-    
-        //         // compare x,y,z with x,y,z in a 3d space
-        //         var d = dist(redSource, greenSource, blueSource, redTarget, greenTarget, blueTarget);
-    
-        //         // if close enough
-        //         if (d < segmentationVal) {
-        //             sumX += x;
-        //             sumY += y;
-        //             matchCount++;
-    
-        //             // if (debug){ // shows matches in fuchsia
-        //               redImg.pixels[index + 0] = 255;
-        //               redImg.pixels[index + 1] = 255;
-        //               redImg.pixels[index + 2] = 255;
-        //             // }
-        //         }
-        //     }
-        // }
-    
-        // if (matchCount>0){
-        //     avgX = sumX / matchCount;
-        //     avgY = sumY / matchCount;
-        // }
-
-
-        // redImg.updatePixels();
-        // image(redImg, 0, imgHeight);
-    
-        // fill(255);
-        // strokeWeight(4);
-        // stroke(255,0,0);
-        // ellipse(avgX, avgY, 16, 16);
-        // strokeWeight(1);
-        // noFill();
-        // stroke(255);
-        // text(threshold, 160, 35);
     }
 
 }
@@ -169,7 +149,52 @@ function greyscaleFilter(img){
     return imgOut;
 }
 
-function channels(img, channel){
+function channel(img, channel)
+{
+        var imgOut = createImage(img.width, img.height);
+        imgOut.loadPixels();
+        img.loadPixels();
+    
+        for (x = 0; x < imgOut.width; x++) {
+            for (y = 0; y < imgOut.height; y++) {
+    
+                var index = (x + y * imgOut.width) * 4;
+    
+                var r = img.pixels[index + 0];
+                var g = img.pixels[index + 1];
+                var b = img.pixels[index + 2];
+
+                var altR, altG, altB
+
+                switch(channel){
+                    case("red"):
+                        altR = r;
+                        altG = 0;
+                        altB = 0;
+                        break;
+                    case("green"): 
+                        altR = 0;
+                        altG = g;
+                        altB = 0;
+                        break;    
+                    case("blue"):
+                        altR = 0;
+                        altG = 0;
+                        altB = b;
+                        break;
+                }
+    
+                imgOut.pixels[index + 0] = altR;
+                imgOut.pixels[index + 1] = altG;
+                imgOut.pixels[index + 2] = altB;
+                imgOut.pixels[index+3]= 255; 
+            }
+        }
+        imgOut.updatePixels();
+        return imgOut;
+}
+
+function channelSegmentation(img, channel){
     // console.log("this is called")
     var imgOut = createImage(img.width, img.height);
     imgOut.loadPixels();
@@ -185,30 +210,33 @@ function channels(img, channel){
             var b = img.pixels[index + 2];
 
             var redTarget = blueTarget = greenTarget = 0
+            var threshold = 0;
 
             switch(channel){
                 case("red"):
+                    threshold = segmentationRVal;
                     redTarget = 255;
                     break;
                 case("green"): 
+                    threshold = segmentationGVal;
                     greenTarget = 255;
                     break;    
                 case("blue"):
+                    threshold = segmentationBVal;
                     blueTarget = 255;
                     break;
             }
 
             var d = dist(r,g,b,redTarget,greenTarget,blueTarget)
-            if(d < 200)
+            if(d < threshold)
             {
                 imgOut.pixels[index + 0] = r;
                 imgOut.pixels[index + 1] = g;
                 imgOut.pixels[index + 2] = b;
             }
             else{
-                var grey = (r + g + b) / 3 * 0.2; // make it dark so the difference in colour is more obvious
+                var grey = (r + g + b) / 3 * 0.1; // make it dark so the difference in colour is more obvious
                 imgOut.pixels[index+0]= imgOut.pixels[index+1] = imgOut.pixels[index+2] = grey;
-
             }
 
             imgOut.pixels[index+3]= 255;
