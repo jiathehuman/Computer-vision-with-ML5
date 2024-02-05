3a780d3b9000b6ea6faf9e3c992e22ceead6bec1
jiathehuman <jiathehuman@gmail.com>
2024-02-05 16:49:18 +0800

    figured out face detection

--- faceDetection.js
+++ faceDetection.js
@@ -1,46 +1,116 @@
-function detectFilter(img) {
-  img.loadPixels();
-  var imgOut = createImage(img.width, img.height);
-  imgOut.copy(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
-  imgOut.loadPixels();
-  // img.loadPixels();
+// function detectFilter(img) {
+//   img.loadPixels();
+//   var imgOut = createImage(img.width, img.height);
+//   imgOut.copy(img, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
+//   imgOut.loadPixels();
 
-  faces = detector.detect(imgOut.canvas);
-//   console.log("detect filter is called");
+//   faces = detector.detect(imgOut.canvas);
+//   for (var i = 0; i < faces.length; i++) {
+//     var face = faces[i];
+//     if (face[4] > 5) {
+//       var firstX = int(face[0]);
+//       var firstY = int(face[1]);
+//       var fwidth = int(face[2]);
+//       var fheight = int(face[3]);
 
-  // console.log(faces.length);
+//       fill(255);
+//       // rect(face[0], face[1], face[2], face[3]);
 
-  for (var i = 0; i < faces.length; i++) {
-    var face = faces[i];
-    if (face[4] > 5) {
-      var startX = int(face[0]);
-      var startY = int(face[1]);
-      var fwidth = int(face[2]);
-      var fheight = int(face[3]);
+//       // ellipse(face[0],20,20)
+//       // ellipse(face[1],20,20)
 
-      fill(255);
-      rect(face[0], face[1], face[2], face[3]);
+//       for(var y = firstY; y < (firstY + fheight); y++)
+//       {
+//         for(var x = firstX; x < (firstX + fwidth); x++)
+//         {
+//           var index = (x + y * imgOut.width) * 4;
+//           ellipse(face[0],20,20)
+//           ellipse(face[1],20,20)
+//         }
+//       }
+//       // ellipse(face[2],20,20)
+//       // ellipse(face[3],20,20)
 
-      for (var y = startY; y < startY + fheight; y++) {
-        for (var x = startX; x < startX + fwidth; x++) {
-          var pixelIndex = (imgOut.width * y + x) * 4;
+//       // for (var y = startY; y < startY + fheight; y++) { 
+//       //   for (var x = startX; x < startX + fwidth; x++) {
+//       //     var pixelIndex = (imgOut.width * y + x) * 4;
 
-          // white box over face
-          // imgOut.pixels[pixelIndex + 0] = 255;
-          // imgOut.pixels[pixelIndex + 1] = 255;
-          // imgOut.pixels[pixelIndex + 2] = 0;
-          // imgOut.pixels[pixelIndex + 3] = 255;
+//       //     // white box over face
+//       //     // imgOut.pixels[pixelIndex + 0] = 255;
+//       //     // imgOut.pixels[pixelIndex + 1] = 255;
+//       //     // imgOut.pixels[pixelIndex + 2] = 0; 
+//       //     // imgOut.pixels[pixelIndex + 3] = 255;
 
-          imgOut.pixels[pixelIndex + 0] = img.pixels[pixelIndex + 0];
-          imgOut.pixels[pixelIndex + 1] = img.pixels[pixelIndex + 1];
-          imgOut.pixels[pixelIndex + 2] = img.pixels[pixelIndex + 2];       
-          imgOut.pixels[pixelIndex + 3] = img.pixels[pixelIndex + 3];
+//       //     imgOut.pixels[pixelIndex + 0] = img.pixels[pixelIndex + 0];
+//       //     imgOut.pixels[pixelIndex + 1] = img.pixels[pixelIndex + 1];
+//       //     imgOut.pixels[pixelIndex + 2] = img.pixels[pixelIndex + 2];       
+//       //     imgOut.pixels[pixelIndex + 3] = img.pixels[pixelIndex + 3];
+//       //   }
+//       //   // console.log("this");
+//       // }
+//     }
+//   }
+
+//   imgOut.updatePixels();
+//   return imgOut;
+// }
+
+function detectFaceFilter(img,faceX, faceY, faceWidth, faceHeight){
+    // console.log("this is called")
+    var imgOut = createImage(img.width, img.height);
+    imgOut.loadPixels();
+    img.loadPixels();
+
+    // console.log(faceX, faceY, faceWidth, faceHeight)
+
+    var startX = int(faceX);
+    var startY = int(faceY)
+    var fwidth = int(faceWidth);
+    var fheight = int(faceHeight);
+
+    console.log(startX, startY, fwidth, fheight);
+
+
+    for (x = 0; x < imgOut.width; x++) {
+
+        for (y = 0; y < imgOut.height; y++) {
+            var index = (x + y * imgOut.width) * 4;
+            var r = img.pixels[index + 0];
+            var g = img.pixels[index + 1];
+            var b = img.pixels[index + 2];
+
+            // push()
+            // translate(0,imgHeight*4)
+            var grey = r * 0.299 + g * 0.587 + b * 0.114; // LUMA ratios 
+
+            imgOut.pixels[index + 0] = r;
+            imgOut.pixels[index + 1] = g;
+            imgOut.pixels[index + 2] = b;
+            imgOut.pixels[index + 3] = 255;
+
+            if((x > startX) && (x < startX + fwidth) && (y > startY) && (y < startY + fheight))
+            {
+                if(selectFilterVal == "greyscale"){
+                    imgOut.pixels[index+0]= imgOut.pixels[index+1] = imgOut.pixels[index+2] = grey * 0.8; // making it slightly darker
+
+                }
+                else if(selectFilterVal == "blur"){
+                    imgOut.pixels[index+0]= imgOut.pixels[index+1] = imgOut.pixels[index+2] = 255; // making it slightly darker
+   
+                }
+                else if(selectFilterVal == "color converted"){
+                    imgOut.pixels[index+0]= imgOut.pixels[index+1] = imgOut.pixels[index+2] = 105; // making it slightly darker
+
+                }
+                else if(selectFilterVal == "pixelate"){
+                    imgOut.pixels[index+0]= imgOut.pixels[index+1] = imgOut.pixels[index+2] = 0; // making it slightly darker
+
+                }
+            }
+
         }
-        // console.log("this");
-      }
     }
-  }
-
-  imgOut.updatePixels();
-  return imgOut;
+    imgOut.updatePixels();
+    return imgOut;
 }
+
--- sketch.js
+++ sketch.js
@@ -28,18 +28,24 @@ var hsvSegmentedImg;
 var ycbcrSegmentedImg;
 
 var segmentationRSlider, segmentationGSlider, segmentationBSlider; 
-var segmentationRVal, segmentationGVal, segmentationBVal;
+var segmentationRVal, segmentationGVal, segmentationBVal, selectFilterVal
 
 var detector;
 var classifier = objectdetect.frontalface;
 
-var faces;
+var faceapi;
+let detections = [];
+
 var imageLoaded = false;
 var testImg;
 
+var faceX, faceY, faceWidth, faceHeight;
+
 ////////////////////////////////////////////////////////////////
 function preload(){
-    img = loadImage("assets/face2.jpg");
+    var scaleFactor = 1.2;
+    var classifier = objectdetect.frontalface;
+    detector = new objectdetect.detector(width, height, scaleFactor, classifier);
 }
 ////////////////////////////////////////////////////////////////
 function setup() {
@@ -48,39 +54,74 @@ function setup() {
     canvas.parent("canvas");
     pixelDensity(1); // makes sure it renders correctly on different screens
 
-    var scaleFactor = 1.2;
-    var classifier = objectdetect.frontalface;
-    detector = new objectdetect.detector(width, height, scaleFactor, classifier);
 
     webcam_stream = createCapture(VIDEO);
-    webcam_stream.hide();
     webcam_stream.size(imgWidth, imgHeight);
+    webcam_stream.hide();
+
+    // img = createImage(imgWidth, imgHeight); 
+    // img.copy(webcam_stream, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
+
 
     segmentationRSlider = createSlider(0, 255, 100);
-
     segmentationGSlider = createSlider(0, 255, 100);
-
     segmentationBSlider = createSlider(0, 255, 100);
 
     segmentationRSlider.parent("redslider");
     segmentationGSlider.parent("greenslider");
     segmentationBSlider.parent("blueslider");
 
+    faceFilterSel = createSelect();
+    faceFilterSel.position(imgWidth * 3 + 20, imgHeight);
+    faceFilterSel.option('greyscale');
+    faceFilterSel.option('blur');
+    faceFilterSel.option('color converted');
+    faceFilterSel.option('pixelate');
+
+    // const faceOptions = {
+    //     withLandMarks: true,
+    //     withExpressions: true, 
+    //     withDescriptor: false, 
+    //     minConfidence: 0.5
+    // }
+
+    // faceapi = ml5.faceApi(webcam_stream, faceOptions, faceLoaded);
 }
+///////////////////////////////////////////////////////////////
+function faceLoaded(){
+    faceapi.detect(gotFaces)
+}
+
+// error handling code
+function gotFaces(error, results){
+    if(error){
+        console.log(error);
+        return;
+    }
+    console.log("got faces is called");
+        detections = results;
+        // faceapi.detect(gotFaces); // continuously obtain RT data
+}
+
 ///////////////////////////////////////////////////////////////
 function draw() {
 
     var count;
     background(0);
-    // var sumX = 0, sumY = 0, avgX = 0, avgY = 0;
-    segmentationRVal = segmentationRSlider.value()
-    segmentationGVal = segmentationGSlider.value()
-    segmentationBVal = segmentationBSlider.value()
+
+
+    // get the values of the sliders
+    segmentationRVal = segmentationRSlider.value();
+    segmentationGVal = segmentationGSlider.value();
+    segmentationBVal = segmentationBSlider.value();
+    selectFilterVal = faceFilterSel.value();
 
 
 
+    // create images and pass them through the filters
     if(imageLoaded)
     {
+
         greyscaleImg = createImage(imgWidth,imgHeight);
         greyscaleImg = greyscaleFilter(img);
 
@@ -115,37 +156,68 @@ function draw() {
         ycbcrSegmentedImg = thresholdFilter(ycbcrImg);
 
         faceImg = createImage(imgWidth, imgHeight);
-        faceImg = detectFilter(img);
+        // faceImg = detectFilter(img);
 
         colorMode(RGB)
         image(img, 0, 0);
         image(greyscaleImg, imgWidth, 0);
 
         // IMPT STUFF, ONLY COMMENTING OUT TO SPEED THINGS UP --------------------------------
-        // image(redImg, 0, imgHeight);
-        // image(greenImg, imgWidth, imgHeight);
-        // image(blueImg, imgWidth * 2, imgHeight);
-        // image(redSegmentImg, 0, imgHeight * 2);
-        // image(greenSegmentImg, imgWidth, imgHeight * 2);
-        // image(blueSegmentImg, imgWidth * 2, imgHeight * 2);
+        image(redImg, 0, imgHeight);
+        image(greenImg, imgWidth, imgHeight);
+        image(blueImg, imgWidth * 2, imgHeight);
+        image(redSegmentImg, 0, imgHeight * 2);
+        image(greenSegmentImg, imgWidth, imgHeight * 2);
+        image(blueSegmentImg, imgWidth * 2, imgHeight * 2);
 
-        // image(img, 0, imgHeight * 3);
-        // image(hsvImg, imgWidth, imgHeight * 3);
-        // image(ycbcrImg, imgWidth * 2, imgHeight * 3);
+        image(img, 0, imgHeight * 3);
+        image(hsvImg, imgWidth, imgHeight * 3);
+        image(ycbcrImg, imgWidth * 2, imgHeight * 3);
 
-        // image(hsvSegmentedImg, imgWidth, imgHeight * 4);
-        // image(ycbcrSegmentedImg, imgWidth * 2, imgHeight * 4);
+        image(hsvSegmentedImg, imgWidth, imgHeight * 4);
+        image(ycbcrSegmentedImg, imgWidth * 2, imgHeight * 4);
 
-        // image(hsvImg, imgWidth, imgHeight * 3);
+        image(hsvImg, imgWidth, imgHeight * 3);
 
         // RMB NOT TO DELETE ----------------------------------------------------------------
 
-        image(faceImg, 0, imgHeight * 4);
-        
-        if(count > 2)
-        {
-            noLoop(); // make sure it does not crash
+        // image(faceImg, 0, imgHeight * 4);
+
+        // push()
+        // translate(0,imgHeight*4)
+        if(detections.length > 0){
+            for(var f = 0; f < detections.length; f++){
+                let x = detections[0].alignedRect._box._x;
+                let y = detections[0].alignedRect._box._y;
+                let rectWidth = detections[0].alignedRect._box.width;
+                let rectHeight = detections[0].alignedRect._box.height;
+
+                let {_x,_y,_width,_height} = detections[0].alignedRect._box;
+    
+                // stroke(255,0,0)
+                // strokeWeight(2)
+                // noFill()
+
+                // faceimg = detectFaceFilter(img);
+                
+                // rect(_x,_y,_width,_height);
+                faceX = detections[0].alignedRect._box._x;
+                faceY = detections[0].alignedRect._box._y;
+                faceWidth = detections[0].alignedRect._box.width;
+                faceHeight = detections[0].alignedRect._box.height;
+                faceImg = detectFaceFilter(img, faceX, faceY, faceWidth, faceHeight);
+                image(faceImg, 0, imgHeight*4)
+            }
         }
+        // pop()
+
+        // faceImg = detectFaceFilter(img, faceX, faceY, faceWidth, faceHeight);
+        // image(faceImg, 0, imgHeight*3)
+
+        // if(count > 2)
+        // {
+        //     noLoop(); // make sure it does not crash
+        // }
     }
    
 }
@@ -155,14 +227,21 @@ function keyPressed()
     if(key == " ")
     {
         img = createImage(imgWidth, imgHeight); 
+        // img.copy(webcam_stream, 0, 0, img.width, img.height, 0, 0, img.width, img.height);
         img = webcam_stream.get();
         imageLoaded = true;
+        const faceOptions = {
+            withLandMarks: true,
+            withExpressions: true, 
+            withDescriptor: false, 
+            minConfidence: 0.5
+        }
+    
+        faceapi = ml5.faceApi(webcam_stream, faceOptions, faceLoaded);
+        // faceapi = ml5.faceApi(img, faceOptions, faceLoaded);
+        // webcam_stream.remove();
     }
 }
-
-
-
-
 
 // Coursera
 // faster method for calculating color similarity which does not calculate root.
