8452621c7fa8b566108b303ffdbeedaaea95b496
jiathehuman <jiathehuman@gmail.com>
2024-02-24 17:31:17 +0800

    added library

--- channelSegmentation.js
+++ channelSegmentation.js
@@ -1,7 +1,13 @@
+/** CHANNEL FUNCTION
+ *  Takes in r, g, b value and a string.
+ *  Suppresses r, g, b according to string.
+ *  Returns an array with only 1 channel with value > 0.
+ */
 function channel(r,g,b, channel)
 {
-    var altR = altG = altB = 0 
+    var altR = altG = altB = 0 // returned r,g,b is 0 by default
 
+    // based on channel, decide which color to not suppress.
     switch(channel){
         case('red'):
             altR = r;
@@ -17,13 +23,22 @@ function channel(r,g,b, channel)
     return [altR, altG, altB, 255];
 }
 
+/** CHANNEL FUNCTION
+ *  Perform threshold on an image with one segment.
+ */
 function channelSegmentation(r,g,b, channel){
+
+    // by default, all targets are 0
     var redTarget = 0
     var blueTarget = 0
     var greenTarget = 0
+
+    // by default, all returned r,g,b are 0
     var altR = altG = altB = 0 
     var threshold = 0;
 
+    // depending on the channel, get the threshold from sliders
+    // target for that color becomes 255
     switch(channel){
         case('red'):
             threshold = segmentationRVal;
@@ -39,14 +54,17 @@ function channelSegmentation(r,g,b, channel){
             break;
     }
 
+    // calculates difference between two colors
     var d = dist(r,g,b,redTarget,greenTarget,blueTarget)
+
+    // if the color is close enough to target, return all original colours.
     if(d < threshold)
     {
         altR = r;
         altG = g;
         altB = b;
     }
-    else{
+    else{ // otherwise if color is far from target
         var dark = (r + g + b) / 3 * 0.1; // make it dark so the difference in colour is more obvious
         altR = altG = altB = dark;
     }
